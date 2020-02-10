fcd103d50440e989b91605d9ac916f5110410f7c
James Balamuta <balamut2@illinois.edu>
2020-02-10 15:54:01 -0600

    Add parameter documentation Enable tuning control

--- R/errum.R
+++ R/errum.R
@@ -2,7 +2,13 @@
 #'
 #' Obtains samples from posterior distributon for the Exploratory
 #' reduced Reparametrized Unified Model (rRUM).
-#' @inheritParams edina
+#'
+#' @param data         Binary responses to assessements in `matrix`
+#'                     form with dimensions \eqn{N \times J}{N x J}.
+#' @param k            Number of Attribute Levels as a positive `integer`.
+#' @param X,v0,v1,cv0,cv1,bnu Additional tuning parameters
+#' @param burnin       Number of Observations to discard on the chain.
+#' @param chain_length Length of the MCMC chain
 #'
 #' @export
 #' @return An `errum` object that has:
@@ -57,8 +63,17 @@
 #' gibbs_estimation = errum(Y, K, burnin, chainLength)
 #' }
 errum = function(data, k = 3,
+                 X = as.matrix(rep(1, ncol(data))),
+                 v0 = 4,
+                 v1 = 2,
+                 cv0 = .1,
+                 cv1 = 10,
+                 bnu = 16,
                  burnin = 10000, chain_length = 10000) {
-    o = errum_Gibbs_Q(data, k, burnin, chain_length)
+
+    o = rRUM_mvnQ_Gibbs(data, k,
+                        X, v0, v1, cv0, cv1, bnu,
+                        burnin, chain_length)
     class(o) = c("errum")
     o
 }
