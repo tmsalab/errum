e8140b25c3750a086bf509d86f5b92efbb74cb8c
James Balamuta <balamut2@illinois.edu>
2020-03-03 22:49:53 -0600

    Fix example

--- R/errum.R
+++ R/errum.R
@@ -29,43 +29,46 @@
 #'
 #' @export
 #' @examples
-#' \dontrun{
 #' # Assign sim helpers
-#' N        = 3000
-#' K        = 3
-#' J        = 30
+#' N = 3000
+#' K = 3
+#' J = 30
 #'
 #' # Sample true attribute profiles
-#' Z         = matrix(rnorm(N*K), N, K)
+#' Z         = matrix(rnorm(N * K), N, K)
 #' Sig       = matrix(.5, K, K)
 #' diag(Sig) = 1
-#' theta     = Z%*%chol(Sig)
+#' theta     = Z %*% chol(Sig)
 #'
-#' thvals    = matrix(qnorm((1:K)/(K+1)),
-#'                    N, K, byrow=T)
+#' thvals    = matrix(qnorm((1:K) / (K + 1)),
+#'                    N, K, byrow = TRUE)
 #'
-#' Alphas    = 1*(theta > thvals)
+#' Alphas    = 1 * (theta > thvals)
 #'
 #' # Defining matrix of possible attribute profiles
 #' As = as.matrix(expand.grid(c(0, 1), c(0, 1), c(0, 1)))
-#' Q = rbind(As[rep(c(2, 3, 5),4),],
-#'           As[rep(c(4, 6, 7),4),],
+#' Q = rbind(As[rep(c(2, 3, 5), 4),],
+#'           As[rep(c(4, 6, 7), 4),],
 #'           As[rep(8, 6),])
 #'
-#' a = As %*% bijectionvector(K)
-#' As = As[a+1,]
+#' # Use simulation functions available in simcdm
+#' if (requireNamespace("simcdm", quietly = TRUE)) {
+#'
+#' a = As %*% simcdm::attribute_bijection(K)
+#' As = As[a + 1,]
 #'
 #' # Setting item parameters
 #' pistar = rep(.9, J)
-#' rstar = matrix(.6, J, K)*Q
+#' rstar = matrix(.6, J, K) * Q
 #'
 #' # Simulate data under rRUM model
-#' Y = sim_rrum(Q, rstar, pistar, Alphas)
+#' Y = simcdm::sim_rrum_items(Q, rstar, pistar, Alphas)
 #'
 #' # Estimation Settings
 #' chainLength = 20000
-#' burnin = chainLength/2 + 1
+#' burnin = chainLength / 2 + 1
 #'
+#' \donttest{
 #' # Gibbs Estimation
 #' gibbs_estimation = errum(Y, K, burnin, chainLength)
 #' }
@@ -77,9 +80,10 @@ errum = function(y, k = 3,
                  cv0 = .1, cv1 = 10,
                  bnu = 16) {
 
-    o = rRUM_mvnQ_Gibbs(data, k,
+    o = rRUM_mvnQ_Gibbs(y, k,
                         X, v0, v1, cv0, cv1, bnu,
                         burnin, chain_length)
+
     class(o) = c("errum")
     o
 }
