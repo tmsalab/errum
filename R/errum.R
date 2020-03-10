be17674ac969dd441e814aa9ef864662931c18c0
James J Balamuta <coatless@users.noreply.github.com>
2020-03-10 12:18:53 -0500

    Address CRAN feedback 3/10 (#7)
    * Cut title name down from 75 characters to 60 chars
    
    * Remove quotes around acronym
    
    * Greatly simplify example for lower runtime...
    
    * Add link to the single estimation function exposed.
    
    * Add simulation disclaimer.
    
    * Update CRAN remarks.

--- R/errum.R
+++ R/errum.R
@@ -30,10 +30,14 @@
 #'
 #' @export
 #' @examples
-#' # Assign sim helpers
-#' N = 3000
+#' # Setup Simulation Parameters
+#' N = 5
 #' K = 3
 #' J = 30
+#' # Note:
+#' # Sample size has been reduced to create a minimally
+#' # viable example that can be run during CRAN's automatic check.
+#' # Please make sure to have a larger sample size of around 3,000.
 #'
 #' # Sample true attribute profiles
 #' Z         = matrix(rnorm(N * K), N, K)
@@ -69,10 +73,8 @@
 #' chainLength = 10000  # Run with 20000
 #' burnin = chainLength / 2
 #'
-#' \donttest{
 #' # Gibbs Estimation
 #' model = errum(Y, K, burnin, chainLength)
-#' }
 #' }
 errum = function(y, k = 3,
                  burnin = 1000, chain_length = 10000,
